c9b80ff4a2fd984e69ec1e1132a96501f39fe5b9
SamuelWebsterIs <84517284+Samuel-Webster-Is-Da-Best@users.noreply.github.com>
2021-06-10 14:40:29 +0000

    stu

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ do {
     alert('Oops, too high')
   } else if (guess < age) {
     alert('Oops, too low')
-  } else {
+  } else if (guess === age){
     alert('You got it!')
     let ans = prompt('Wish to play agin? (y/n)')
     if (ans === 'y') {
       age = Math.floor(Math.random() * 30 + 1)
       guess = 0
+    } else {
+      alert('input real number please')
     }
   }
 } while (age != guess)
